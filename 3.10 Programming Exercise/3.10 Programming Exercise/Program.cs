c2456c069d18aacedeca0fb6362f012a94e0974d
Yasir Hassan <13193862+yasirh77@users.noreply.github.com>
2020-01-29 10:31:29 -0800

    Add files via upload

---
+++ 3.10 Programming Exercise/3.10 Programming Exercise/Program.cs
@@ -0,0 +1,20 @@
+﻿# include <iostream>
+#define PI 3.1416
+using namespace std;
+int main()
+{
+    float radius, area, circumference;
+
+    cout << "Please input the value of the radius: ";
+    cin >> radius;
+
+    circumference = 2 * PI * radius;
+    area = PI * (radius * radius);
+
+    cout << "The area of the circle is " << area << " and the circumference is "
+        << circumference << '.' << endl;
+
+    cout << endl;
+
+    return 0;
+}
